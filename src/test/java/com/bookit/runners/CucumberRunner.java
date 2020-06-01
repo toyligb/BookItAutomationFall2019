f72e60ad378cd7abbfc1a39f796da31d910b9f2a
Toyly Gurdov <toyligb@gmail.com>
2020-05-31 17:43:14 -0700

    last framework

---
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -0,0 +1,19 @@
+package com.bookit.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/bookit/step_definitions",
+        features = "src/test/resources/features",
+        plugin = {
+                "json:target/cucumber.json"
+        },
+//        tags = "@create_student_3",
+        dryRun = false
+
+)
+public class CucumberRunner {
+}
---
+++ src/test/java/com/bookit/units/APIUtilitiesUnitTests.java
@@ -0,0 +1,35 @@
+package com.bookit.units;
+
+import com.bookit.utilities.APIUtilities;
+import com.bookit.utilities.Environment;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Class with unit tests for APIUtilities class
+ * Here we ensure that utilities work fine before using hem in action
+ */
+public class APIUtilitiesUnitTests {
+
+
+    @Test
+    public void getTokenTest(){
+        String token = APIUtilities.getToken();
+        String tokenForStudent = APIUtilities.getToken("student");
+        String tokenForTeacher = APIUtilities.getToken("teacher");
+
+        Assert.assertNotNull(token);
+        Assert.assertNotNull(tokenForStudent);
+        Assert.assertNotNull(tokenForTeacher);
+    }
+
+    @Test
+    public void testIfUserExists(){
+        int actual = APIUtilities.getUserID("thereisnoemaillikethis@email.com", "123123");
+        Assert.assertEquals(-1, actual);//negative test
+
+        int actual2 = APIUtilities.getUserID(Environment.MEMBER_USERNAME, Environment.MEMBER_PASSWORD);
+        //positive test
+        Assert.assertTrue(actual2 > 0);//if ID is positive - user exists indeed, otherwise it return -1
+    }
+}
