acbeeb6881eefe44a6b27d6f0df19fe7f1912d73
Toyly Gurdov <toyligb@gmail.com>
2020-06-01 19:26:41 -0700

    db validations

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com/bookit/step_definitions",
-        features = "src/test/resources/features",
+        glue = "com\\bookit\\step_definitions",
+        features = "src\\test\\resources\\features",
         plugin = {
-                "json:target/cucumber.json"
+                "json:target\\cucumber.json"
         },
 //        tags = "@create_student_3",
         dryRun = false
